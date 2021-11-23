d9cf879167e904930d627fa9def8c13cef8b9503
nahuel7212 <nahuelpereira7212@gmail.com>
2021-11-23 19:03:21 -0300

    MVC

---
+++ PresentacionInicio/Controllers/InicioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentacionInicio.Controllers
+{
+    public class InicioController : Controller
+    {
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        public ActionResult Envios()
+        {
+            return Redirect("https://localhost:44361/");
+        }
+
+    }
+}
